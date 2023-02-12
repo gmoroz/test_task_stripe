a87cbeacaf132378a181e192c93d8f3603ec677e
gmoroz <12345hasek@gmail.com>
2023-02-12 17:01:26 +0300

    fix: Secret key variable

--- test_task_stripe/settings.py
+++ test_task_stripe/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--m)&f3-=ker2qsjpm@b@idq*t#$yf83n%tu8x4ui0qf=f1suo8'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
