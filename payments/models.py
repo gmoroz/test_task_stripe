17fa43de45aa3197d3369bf1f9a67220bee0bd5b
gmoroz <12345hasek@gmail.com>
2023-02-12 18:11:17 +0300

    feat: add str and get_price funcs

--- payments/models.py
+++ payments/models.py
@@ -7,3 +7,10 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.PositiveIntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.name
+
+    @property
+    def get_price(self) -> str:
+        return f'{self.price // 100} $'
