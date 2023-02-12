6c9e234e7598ae6e59d96160cee94e52005833db
gmoroz <12345hasek@gmail.com>
2023-02-12 17:46:38 +0300

    feat: add create checkout session

--- payments/views.py
+++ payments/views.py
@@ -1,1 +1,32 @@
 from __future__ import annotations
+
+import stripe
+from django.conf import settings
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+stripe.api_key = settings.STRIPE_PRIVATE_KEY
+YOUR_DOMAIN = 'http://127.0.0.1:8000'
+
+
+@csrf_exempt
+def create_checkout_session(request):
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[
+            {
+                'price_data': {
+                    'currency': 'inr',
+                    'product_data': {
+                        'name': 'Test item',
+                    },
+                    'unit_amount': 10000,
+                },
+                'quantity': 1,
+            },
+        ],
+        mode='payment',
+        success_url=YOUR_DOMAIN + '/success.html',
+        cancel_url=YOUR_DOMAIN + '/cancel.html',
+    )
+    return JsonResponse({'id': session.id})
