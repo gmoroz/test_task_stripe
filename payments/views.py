ae14ad58fa03da4872c024c477c9809f3ae5f0ef
gmoroz <12345hasek@gmail.com>
2023-02-12 18:11:43 +0300

    feat: add session create view

--- payments/views.py
+++ payments/views.py
@@ -3,30 +3,36 @@ from __future__ import annotations
 import stripe
 from django.conf import settings
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import DetailView
 
-stripe.api_key = settings.STRIPE_PRIVATE_KEY
-YOUR_DOMAIN = 'http://127.0.0.1:8000'
+from .models import Item
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
-@csrf_exempt
-def create_checkout_session(request):
-    session = stripe.checkout.Session.create(
-        payment_method_types=['card'],
-        line_items=[
-            {
-                'price_data': {
-                    'currency': 'inr',
-                    'product_data': {
-                        'name': 'Test item',
+class SessionCreateView(DetailView):
+    def get(self, request, *args, **kwargs):
+        try:
+            item = self.get_object()
+        except Item.DoesNotExist:
+            return JsonResponse({'error': 'Not found'}, status=404)
+
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'inr',
+                        'product_data': {
+                            'name': f'{item.name}',
+                        },
+                        'unit_amount': item.get_price,
                     },
-                    'unit_amount': 10000,
+                    'quantity': 1,
                 },
-                'quantity': 1,
-            },
-        ],
-        mode='payment',
-        success_url=YOUR_DOMAIN + '/success.html',
-        cancel_url=YOUR_DOMAIN + '/cancel.html',
-    )
-    return JsonResponse({'id': session.id})
+            ],
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success.html',
+            cancel_url=YOUR_DOMAIN + '/cancel.html',
+        )
+        return JsonResponse({'id': session.id})
