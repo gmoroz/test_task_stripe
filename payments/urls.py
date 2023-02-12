f1441d3a40c9d6a2ce3ea7163e963992d5cc9003
gmoroz <12345hasek@gmail.com>
2023-02-12 19:33:19 +0300

    fix: add items context

--- payments/urls.py
+++ payments/urls.py
@@ -4,14 +4,23 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from . import views
+from .models import Item
 
 urlpatterns = [
     path('buy/<int:pk>', views.SessionCreateView.as_view(), name='checkout'),
-    path('cancel/', TemplateView.as_view(template_name='cancel.html', name='cancel')),
+    path('cancel/', TemplateView.as_view(template_name='cancel.html'), name='cancel'),
     path(
-        'success/', TemplateView.as_view(
-            template_name='success.html', name='success',
+        'success/',
+        TemplateView.as_view(
+            template_name='success.html',
         ),
+        name='success',
     ),
-    path('', TemplateView.as_view(template_name='items.html')),
+    path(
+        '',
+        TemplateView.as_view(
+            template_name='items.html', extra_context={'items': Item.objects.all()},
+        ),
+        name='items',
+    ),
 ]
