99c814aa332d25c88ca4b8e30f1af70876c8f6f2
gmoroz <12345hasek@gmail.com>
2023-02-12 16:59:42 +0300

    feat: add Item model

--- payments/models.py
+++ payments/models.py
@@ -1,1 +1,9 @@
 from __future__ import annotations
+
+from django.db import models
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.PositiveIntegerField(default=0)
